adb65df47ae69b7fa6f05c777b14f05a0e230b3b
Rava-tech <jeremiasz206@gmail.com>
2023-06-10 03:37:37 +0200

    register, login, logout, profile

--- Let_sit_together_2/Let_sit_together_2/urls.py
+++ Let_sit_together_2/Let_sit_together_2/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Lets_sit_together_4 import views
+from django.contrib.auth.views import LoginView, LogoutView
+from Lets_sit_together_4.views import register, search, welcome_view, logout_view, profile_view, \
+    search_map, search_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    path('search/', search, name='search'),
+    path('welcome/', welcome_view, name='welcome'),
+    path('logout/', logout_view, name='logout'),
+    path('accounts/profile/', profile_view, name='profile'),
+    path('map/', search_map, name='map'),
+    path('search_results/', search_view, name='search_results'),
 ]
--- Let_sit_together_2/Lets_sit_together_4/views.py
+++ Let_sit_together_2/Lets_sit_together_4/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # Dodaj komunikat o sukcesie
+            messages.success(request, 'Rejestracja zakończona pomyślnie. Witamy!')
+            return redirect('welcome')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def search(request):
+    places = request.GET.get('places')
+    distance = request.GET.get('distance')
+
+    # Przetwarzanie wyszukiwania i wykonanie odpowiednich działań
+    # ...
+
+    return render(request, 'search.html')
+
+def welcome_view(request):
+    return render(request, 'welcome.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        # Logika uwierzytelniania użytkownika
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Przekierowanie użytkownika na stronę główną
+            return redirect('welcome.html')  # 'home.html' to nazwa widoku dla strony głównej
+        else:
+            # Obsługa błędnego uwierzytelniania
+            pass
+    else:
+        # Renderowanie formularza logowania
+        pass
+
+def profile_view(request):
+    # Logika wyświetlania profilu użytkownika
+    return render(request, 'profile.html', {'user': request.user})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home.html')
+
+def search_map(request):
+    if request.method == 'POST':
+        # Przetwarzanie danych z formularza
+        radius = request.POST.get('radius')  # Przykładowe pole formularza
+
+        # Przygotowanie kontekstu dla szablonu
+        context = {
+            'radius': radius,
+        }
+
+        return render(request, 'map.html', context)
+    else:
+        return render(request, 'search.html')
+
+def search_view(request):
+    # Przetwarzanie danych i przygotowanie wyników
+    # ...
+    return redirect('search_results')
